48534591ee126a8f99bdaf5973ca7b1cf1b60745
Mohd-Yasir <mohdxyasir@gmail.com>
2024-11-03 17:02:35 +0530

    signup page completed

--- lib/view/signup.dart
+++ lib/view/signup.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 import 'package:shimmer/shimmer.dart';
-
 import 'login.dart';
 
 class Signup extends StatelessWidget {
